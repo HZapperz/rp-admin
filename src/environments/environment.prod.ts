af05608cb85d0c90585432cba682d900bf78feb1
hzapperz <hamza.awty@gmail.com>
2025-12-17 22:17:24 -0600

    bruh

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'https://www.royalpawzusa.com',
-  mapboxAccessToken: 'pk.eyJ1IjoiaHphcHBlcnoiLCJhIjoiY21oOGh4c3ZzMHgxYTJrb2E4dW5jcmxiOSJ9.v-SWnWwRo7-Ypir_nzCcSQ',
+  mapboxAccessToken: 'pk.eyJ1IjoiaHphcHBlcnoiLCJhIjoiY21oOGh4c3ZzMHgxbTJrb2E4dW5jcmxiOSJ9.v-SWnWwRo7-Ypir_nzCcSQ',
   supabase: {
     url: 'https://api.royalpawzusa.com',
     anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJqZXdqdGF6cmV6bGt5ZW1kdWtpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAyMDc4OTEsImV4cCI6MjA3NTc4Mzg5MX0.x0BOwZByyxUdlyJqXPdfiuFYF3U94Le06Fc1eGabOvM',
